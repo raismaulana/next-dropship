fd4c0198f8e630c9af96af7591a45d9cf15e9c17
raismaulana <lufriraismaulana@gmail.com>
2024-09-14 11:36:50 +0700

    add database_extra

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -78,6 +78,12 @@ const projectConfig = {
   admin_cors: ADMIN_CORS,
   // Uncomment the following lines to enable REDIS
   // redis_url: REDIS_URL
+  database_extra: process.env.NODE_ENV !== "development" ?
+    {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    } : {},
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
